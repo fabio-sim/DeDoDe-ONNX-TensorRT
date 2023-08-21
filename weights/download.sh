b5cff4bc0b370269e378bea35424744fe71b0580
fabio-sim <54187389+fabio-sim@users.noreply.github.com>
2023-08-21 21:34:15 +0700

    Fix download script

--- weights/download.sh
+++ weights/download.sh
@@ -3,13 +3,13 @@
 RELEASE=v1.0.0
 NUM_KEYPOINTS=1024
 
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}_fp16.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}_fp16.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}_trt.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}_trt.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}_fp16.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}_fp16.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/dedode_end2end_${NUM_KEYPOINTS}_trt.onnx -o weights/dedode_end2end_${NUM_KEYPOINTS}_trt.onnx
 
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/detector_L_${NUM_KEYPOINTS}.onnx -o weights/detector_L_${NUM_KEYPOINTS}.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/detector_L_${NUM_KEYPOINTS}_fp16.onnx -o weights/detector_L_${NUM_KEYPOINTS}_fp16.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/descriptor_B_${NUM_KEYPOINTS}.onnx -o weights/descriptor_B_${NUM_KEYPOINTS}.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/descriptor_B_${NUM_KEYPOINTS}_fp16.onnx -o weights/descriptor_B_${NUM_KEYPOINTS}_fp16.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/matcher.onnx -o weights/matcher.onnx
-curl https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/matcher_fp16.onnx -o weights/matcher_fp16.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/detector_L_${NUM_KEYPOINTS}.onnx -o weights/detector_L_${NUM_KEYPOINTS}.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/detector_L_${NUM_KEYPOINTS}_fp16.onnx -o weights/detector_L_${NUM_KEYPOINTS}_fp16.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/descriptor_B_${NUM_KEYPOINTS}.onnx -o weights/descriptor_B_${NUM_KEYPOINTS}.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/descriptor_B_${NUM_KEYPOINTS}_fp16.onnx -o weights/descriptor_B_${NUM_KEYPOINTS}_fp16.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/matcher.onnx -o weights/matcher.onnx
+curl -L https://github.com/fabio-sim/DeDoDe-ONNX-TensorRT/releases/download/${RELEASE}/matcher_fp16.onnx -o weights/matcher_fp16.onnx
